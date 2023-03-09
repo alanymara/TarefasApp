abc5361c0fb87e2db03a7148c9805032f4eac6eb
Alany <alanymara@gmail.com>
2023-03-09 20:47:04 -0300

    fix: Alterando tipo de status para enum

--- SistemaDeTarefas/Models/TarefaModel.cs
+++ SistemaDeTarefas/Models/TarefaModel.cs
@@ -1,11 +1,13 @@
-﻿namespace SistemaDeTarefas.Models
+﻿using SistemaDeTarefas.Enums;
+
+namespace SistemaDeTarefas.Models
 {
     public class TarefaModel
     {
         public int Id { get; set; }
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
-        public int Status { get; set; }
+        public StatusTarefa Status { get; set; }
 
     }
 }
